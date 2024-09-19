4668004acb604522bc06c91f9778ceff14d4f240
BaltazarJim <rebajiru@gmail.com>
2024-09-19 13:17:54 -0600

    Template
    Template

---
+++ ASSEMBLER/32MC202/Template.X/nbproject/Makefile-default.mk
@@ -0,0 +1,153 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=newmainasm33.s
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/newmainasm33.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/newmainasm33.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/newmainasm33.o
+
+# Source Files
+SOURCEFILES=newmainasm33.s
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=33FJ32MC202
+MP_LINKER_FILE_OPTION=,--script="..\..\p33FJ32MC202.gld"
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/newmainasm33.o: newmainasm33.s  .generated_files/flags/default/9f50750e747ab484cb7ddcb8cdd0d4402b8cbc59 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/newmainasm33.o.d 
+	@${RM} ${OBJECTDIR}/newmainasm33.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  newmainasm33.s  -o ${OBJECTDIR}/newmainasm33.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -DXPRJ_default=$(CND_CONF)    -Wa,-MD,"${OBJECTDIR}/newmainasm33.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)  -mdfp="${DFP_DIR}/xc16"
+	
+else
+${OBJECTDIR}/newmainasm33.o: newmainasm33.s  .generated_files/flags/default/db1da9d804dbfdbdfb86024cb27176c1cdafe53c .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/newmainasm33.o.d 
+	@${RM} ${OBJECTDIR}/newmainasm33.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  newmainasm33.s  -o ${OBJECTDIR}/newmainasm33.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -DXPRJ_default=$(CND_CONF)    -Wa,-MD,"${OBJECTDIR}/newmainasm33.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)  -mdfp="${DFP_DIR}/xc16"
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    ../../p33FJ32MC202.gld
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)      -Wl,,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	
+else
+${DISTDIR}/Template.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   ../../p33FJ32MC202.gld
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/Template.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	${MP_CC_DIR}\\xc16-bin2hex ${DISTDIR}/Template.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a    -mdfp="${DFP_DIR}/xc16" 
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
---
+++ ASSEMBLER/32MC202/Template.X/newmainasm33.s
@@ -0,0 +1,145 @@
+/*
+ * File:   %<%NAME%>%.%<%EXTENSION%>%
+ * Author: %<%USER%>%
+ *
+ * Created on %<%DATE%>%, %<%TIME%>%
+ */
+
+    .include "p33fj32mc202.inc"
+
+    ; _____________________Configuration Bits_____________________________
+    ;User program memory is not write-protected
+    #pragma config __FGS, GWRP_OFF & GSS_OFF & GCP_OFF
+    
+    ;Internal Fast RC (FRC)
+    ;Start-up device with user-selected oscillator source
+    #pragma config __FOSCSEL, FNOSC_FRC & IESO_ON
+    
+    ;Both Clock Switching and Fail-Safe Clock Monitor are disabled
+    ;XT mode is a medium-gain, medium-frequency mode that is used to work with crystal
+    ;frequencies of 3.5-10 MHz
+  ; #pragma config __FOSC, FCKSM_CSDCMD & POSCMD_XT
+    
+    ;Watchdog timer enabled/disabled by user software
+    #pragma config __FWDT, FWDTEN_OFF
+    
+    ;POR Timer Value
+    #pragma config __FPOR, FPWRT_PWR128
+   
+    ; Communicate on PGC1/EMUC1 and PGD1/EMUD1
+    ; JTAG is Disabled
+    #pragma config __FICD, ICS_PGD1 & JTAGEN_OFF
+
+;..............................................................................
+;Program Specific Constants (literals used in code)
+;..............................................................................
+
+    .equ SAMPLES, 64         ;Number of samples
+
+
+
+;..............................................................................
+;Global Declarations:
+;..............................................................................
+
+    .global _wreg_init       ;Provide global scope to _wreg_init routine
+                                 ;In order to call this routine from a C file,
+                                 ;place "wreg_init" in an "extern" declaration
+                                 ;in the C file.
+
+    .global __reset          ;The label for the first line of code.
+
+;..............................................................................
+;Constants stored in Program space
+;..............................................................................
+
+    .section .myconstbuffer, code
+    .palign 2                ;Align next word stored in Program space to an
+                                 ;address that is a multiple of 2
+ps_coeff:
+    .hword   0x0002, 0x0003, 0x0005, 0x000A
+
+
+
+
+;..............................................................................
+;Uninitialized variables in X-space in data memory
+;..............................................................................
+
+    .section .xbss, bss, xmemory
+x_input: .space 2*SAMPLES        ;Allocating space (in bytes) to variable.
+
+
+
+;..............................................................................
+;Uninitialized variables in Y-space in data memory
+;..............................................................................
+
+    .section .ybss, bss, ymemory
+y_input:  .space 2*SAMPLES
+
+
+
+
+;..............................................................................
+;Uninitialized variables in Near data memory (Lower 8Kb of RAM)
+;..............................................................................
+
+    .section .nbss, bss, near
+var1:     .space 2               ;Example of allocating 1 word of space for
+                                 ;variable "var1".
+
+
+
+
+;..............................................................................
+;Code Section in Program Memory
+;..............................................................................
+
+.text                             ;Start of Code section
+__reset:
+    MOV #__SP_init, W15       ;Initalize the Stack Pointer
+    MOV #__SPLIM_init, W0     ;Initialize the Stack Pointer Limit Register
+    MOV W0, SPLIM
+    NOP                       ;Add NOP to follow SPLIM initialization
+
+    CALL _wreg_init           ;Call _wreg_init subroutine
+                                  ;Optionally use RCALL instead of CALL
+
+
+
+
+        ;<<insert more user code here>>
+
+
+;SETM    AD1PCFGL  ;PORTB AS DIGITAL
+;CLR	TRISB
+SETM	W10
+
+	
+done:	    ;INFINITE LOOP
+    
+    COM	    W10,    W10
+    
+    BRA     done              ;Place holder for last line of executed code
+
+
+
+;..............................................................................
+;Subroutine: Initialization of W registers to 0x0000
+;..............................................................................
+
+_wreg_init:
+    CLR W0
+    MOV W0, W14
+    REPEAT #12
+    MOV W0, [++W14]
+    CLR W14
+    RETURN
+
+
+
+
+;--------End of All Code Sections ---------------------------------------------
+
+.end                               ;End of program code in this file
